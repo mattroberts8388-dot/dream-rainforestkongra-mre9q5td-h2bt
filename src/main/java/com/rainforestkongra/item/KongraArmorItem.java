fe3dd6629ea22146f92b42e33b1977c6b2c4f694
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-09 21:40:07 -0400

    Add src/main/java/com/rainforestkongra/item/KongraArmorItem.java

---
+++ src/main/java/com/rainforestkongra/item/KongraArmorItem.java
@@ -0,0 +1,10 @@
+package com.rainforestkongra.item;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class KongraArmorItem extends ArmorItem {
+    public KongraArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+}
