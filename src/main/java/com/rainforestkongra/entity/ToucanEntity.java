493f5f79c101f447b3d3a77bcc1b9c9d804a5b02
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-09 21:40:04 -0400

    Add src/main/java/com/rainforestkongra/entity/ToucanEntity.java

---
+++ src/main/java/com/rainforestkongra/entity/ToucanEntity.java
@@ -0,0 +1,65 @@
+package com.rainforestkongra.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.AnimalMateGoal;
+import net.minecraft.entity.ai.goal.FollowParentGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class ToucanEntity extends AnimalEntity {
+    public ToucanEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public static DefaultAttributeContainer.Builder createToucanAttributes() {
+        return AnimalEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 6.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.28);
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(1, new AnimalMateGoal(this, 1.0));
+        this.goalSelector.add(2, new FollowParentGoal(this, 1.1));
+        this.goalSelector.add(3, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 6.0f));
+        this.goalSelector.add(5, new LookAroundGoal(this));
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_PARROT_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(net.minecraft.entity.damage.DamageSource source) {
+        return SoundEvents.ENTITY_PARROT_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_PARROT_DEATH;
+    }
+
+    @Override
+    public boolean isBreedingItem(net.minecraft.item.ItemStack stack) {
+        return stack.isOf(net.minecraft.item.Items.MELON_SEEDS) || stack.isOf(net.minecraft.item.Items.WHEAT_SEEDS);
+    }
+
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return com.rainforestkongra.registry.ModEntities.TOUCAN.create(world);
+    }
+}
